ad600d9135935249c9d7e577bb10c0cff7d5a97c
Viraj <virajjain7.vj@gmail.com>
2022-08-21 00:16:06 +0530

    routing to different page done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Login from "./components/LoginPage/Login";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Poll from "./components/HomePage/Poll";
+import Result from "./components/HomePage/Result";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-
+        <Route path="/home" element={<Poll />} />
+        <Route path="/home/result" element={<Result />} />
         <Route path="*" element={<Navigate to={"/"} />} />
       </Routes>
     </BrowserRouter>

64cc86caa92c93d3ef88befdcc4039c4ff029c75
Viraj <virajjain7.vj@gmail.com>
2022-08-23 23:45:16 +0530

    fix: display text if no points table present

--- src/components/HomePage/ResultPage/PointsTable.js
+++ src/components/HomePage/ResultPage/PointsTable.js
@@ -4,35 +4,35 @@ const PointsTable = ({ DishData }) => {
   console.log("DishData", DishData);
   return (
     <>
-      <section className=" bg-gray-400 text-gray-600  px-4">
-        <div className="w-full max-w-2xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
-          <header className="px-5 py-4 border-b border-gray-100">
-            <h2 className="font-semibold text-lg tracking-wide uppercase text-gray-800 text-center">
-              Points Table
-            </h2>
-          </header>
-          <div className="p-3">
-            <div className="overflow-x-auto">
-              <table className="table-auto w-full">
-                <thead className="text-xs font-semibold uppercase text-gray-500 bg-gray-50">
-                  <tr>
-                    <th className="p-2 whitespace-nowrap">
-                      <div className="font-semibold text-left">sl.no</div>
-                    </th>
-                    <th className="p-2 whitespace-nowrap">
-                      <div className="font-semibold text-left">id</div>
-                    </th>
-                    <th className="p-2 whitespace-nowrap">
-                      <div className="font-semibold text-left">dish</div>
-                    </th>
-                    <th className="p-2 whitespace-nowrap">
-                      <div className="font-semibold text-center">points</div>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="text-sm divide-y divide-gray-100">
-                  {DishData.length > 0 &&
-                    DishData.map(([dishName, points], index) => (
+      {DishData.length > 0 ? (
+        <>
+          <div className="w-full max-w-2xl mx-auto text-gray-600 px-4 bg-white shadow-lg rounded-sm border border-gray-200">
+            <header className="px-5 py-4 border-b border-gray-200">
+              <h2 className="font-semibold text-lg tracking-wide uppercase text-gray-800 text-center">
+                Points Table
+              </h2>
+            </header>
+            <div className="p-3">
+              <div className="overflow-x-auto">
+                <table className="table-auto w-full">
+                  <thead className="text-xs font-semibold uppercase text-gray-500 bg-gray-50">
+                    <tr>
+                      <th className="p-2 whitespace-nowrap">
+                        <div className="font-semibold text-left">sl.no</div>
+                      </th>
+                      <th className="p-2 whitespace-nowrap">
+                        <div className="font-semibold text-left">id</div>
+                      </th>
+                      <th className="p-2 whitespace-nowrap">
+                        <div className="font-semibold text-left">dish</div>
+                      </th>
+                      <th className="p-2 whitespace-nowrap">
+                        <div className="font-semibold text-center">points</div>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="text-sm divide-y divide-gray-100">
+                    {DishData.map(([dishName, points], index) => (
                       <>
                         <tr key={index}>
                           <td className="p-2 whitespace-nowrap">
@@ -56,12 +56,21 @@ const PointsTable = ({ DishData }) => {
                         </tr>
                       </>
                     ))}
-                </tbody>
-              </table>
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </>
+      ) : (
+        <>
+          <div className="flex justify-center">
+            <h1 className="text-2xl text-gray-800">
+              Please rate our dishes first.
+            </h1>
+          </div>
+        </>
+      )}
     </>
   );
 };
